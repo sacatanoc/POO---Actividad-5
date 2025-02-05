244eb1baa42dedc1c12c993745a9e06566a1239b
Samuel Cataño C <Samuel Catao C@LAPTOP-H1JKSR5M>
2025-02-04 19:07:22 -0500

    Actividad 5 - Ejercicio 1 - Leonardo Bermón Angarita - Capítulo 8 - Ejercicio 2

---
+++ src/main/java/Notas/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Notas;
+
+/**
+ *
+ * @author Samuel Cataño C
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+    }
+}
